714a5aa8cce3d74c842ab70ebe47745d2edab110
Farooq Mahmud <farooq_am@hotmail.com>
2019-05-13 17:45:59 -0700

    wip

---
+++ ProductShop/Login/config.js
@@ -0,0 +1,18 @@
+const config = {};
+
+config.endpoint = 'https://productshopfarooq.documents.azure.com:443/';
+config.masterKey = '2zg1wP9W3rKQz2FwcKqWt8SPqQAzQe9OyhftrqhWudC5BzSATRzVVw1k8pl2ZiRInIShnlweLCvpuMSUKuaZxg==';
+
+config.database = {
+    id: 'ProductShop',
+};
+
+config.productCollection = {
+    id: 'Products',
+};
+
+config.userLoginsCollection = {
+    id: 'UserLogins',
+};
+
+module.exports = config;
