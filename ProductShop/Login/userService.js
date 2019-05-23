b32d5f7d6e740e98052268fb41c541174fe6d059
farooqam <farooq_am@hotmail.com>
2019-05-23 16:55:31 -0700

    API ARM template (#9)

---
+++ ProductShop/Login/userService.js
@@ -0,0 +1,28 @@
+const { CosmosClient } = require('@azure/cosmos');
+const config = require('./config.json');
+
+const { endpoint, masterKey } = process.env;
+const client = new CosmosClient({ endpoint, auth: { masterKey } });
+const databaseId = config.database.id;
+const userLoginsCollection = config.userLoginsCollection.id;
+
+const addUserLogin = async (user) => {
+    const userLogin = {
+        username: user.username,
+        password: user.password,
+        location: 'US',
+        loginTime: new Date().getTime(),
+    };
+
+    const { item } = await client
+        .database(databaseId)
+        .container(userLoginsCollection)
+        .items
+        .create(userLogin);
+
+    return item;
+};
+
+module.exports = {
+    addUserLogin,
+};
